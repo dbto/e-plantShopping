c570241f7c1cc4a4e4cfedffa81044d0c8592b80
dbto <jens.lechner.dbto@googlemail.com>
2025-08-29 12:17:57 -0400

    add checkout button functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -24,7 +24,8 @@ const CartItem = ({ onRemoveFromCart, onContinueShopping }) => {
   };
 
   const handleCheckoutShopping = (e) => {
-      alert('Functionality to be added for future reference');
+    e.preventDefault();
+    alert('Functionality to be added for future reference');
   };
 
   const handleIncrement = (item) => {
@@ -76,7 +77,7 @@ const CartItem = ({ onRemoveFromCart, onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
