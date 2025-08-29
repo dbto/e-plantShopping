3cdd1da3b22dac1b1ab0d8e8bd49012f9eec6e16
dbto <jens.lechner.dbto@googlemail.com>
2025-08-29 10:57:10 -0400

    adjust CartItem component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    
+    cart.forEach(item => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+        //total += item.quantity;
+    });
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+      alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    const quantity = item.quantity;
+    const name = item.name;
+    if (quantity > 1) {
+        dispatch(updateQuantity({ name: name, quantity: quantity - 1 }));
+    } else {
+        dispatch(removeItem(name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
